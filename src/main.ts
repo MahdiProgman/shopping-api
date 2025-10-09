cbf1d6e520b0fcbcb30e2d3f5dcede4cc851c2b5
MahdiProgman <mahdihabibkah78@gmail.com>
2025-10-09 10:47:13 +0330

    chore(swagger): configured swagger on project and some changes
    - set a global prefix for routes
    - enabled versioning on project
    - fixed path problem in applying generate-request-id-middleware
    - installed @nestjs/swagger package
    - configured swagger on project
    - added `Documentaton` and  `How to Run Tests` section in README.md

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,32 @@ import { LoggerService } from './core/logger/contracts/logger.service';
 import { LOGGER_SERVICE } from './core/logger/constants';
 import { GlobalExceptionFilter } from './core/app/filters/global-exception.filter';
 import { SuccessResponseInterceptor } from './core/app/interceptors/success-response.interceptor';
+import { VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get<ConfigService>(CONFIG_SERVICE);
   const loggerService = app.get<LoggerService>(LOGGER_SERVICE);
+
+  app.setGlobalPrefix('api');
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Shopping API')
+    .setDescription('Documentation for best of the best')
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/docs', app, document);
 
   app.useGlobalFilters(new GlobalExceptionFilter());
 
