0cbbca6b59851f40aa4883d05a8250ae3a270014
MahdiProgman <mahdihabibkah78@gmail.com>
2025-10-16 11:19:34 +0330

    fix: added prisma-module to imports of user-module to help repositories get the prisma-service

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { SESSION_REPOSITORY, USER_REPOSITORY } from './constants';
 import { UserRepository } from './infrastructure/database/repositories/user.repository';
 import { SessionRepository } from './infrastructure/database/repositories/session.repository';
+import { PrismaModule } from 'src/core/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [
     {
       provide: USER_REPOSITORY,
